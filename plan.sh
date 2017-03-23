a498fec2738ebbb4ba6cf27cda015ec29cc84293
Lamont Lucas <lamont@fastrobot.com>
2017-03-23 01:41:16 -0700

    Initial (non-working) attempt from the habitat.sh demo event

---
+++ plan.sh
@@ -0,0 +1,35 @@
+pkg_name=prometheus
+pkg_description="Prometheus monitoring"
+pkg_upstream_url=http://prometheus.io
+pkg_origin=core
+pkg_version=1.5.2
+pkg_maintainer="Lamont Lucas <lamont@fastrobot.com>"
+pkg_license=('Apache-2.0')
+pkg_source=https://github.com/prometheus/prometheus/releases/download/v${pkg_version}/prometheus-${pkg_version}.linux-amd64.tar.gz
+pkg_shasum=971c5f365c3f53f52d05729acf43962905832b33d740798094850a25645de5ae
+pkg_dirname=${pkg_name}-${pkg_version}
+pkg_deps=(core/go)
+pkg_build_deps=(core/tar)
+#pkg_lib_dirs=(lib)
+#pkg_include_dirs=(include)
+pkg_exports=(
+  [port-http]=ports.http
+)
+pkg_exposes=(port-http)
+pkg_bin_dirs=(prometheus)
+pkg_svc_run="prometheus -config.file=$pkg_prefix/prometheus.yml"
+
+do_unpack() {
+  cd "${HAB_CACHE_SRC_PATH}" || exit
+  tar zxf ${pkg_filename}
+}
+
+do_build() {
+  return 0
+}
+
+do_install() {
+  mkdir ${pkg_prefix}/prometheus || true
+  cp ${HAB_CACHE_SRC_PATH}/${pkg_dirname}.linux-amd64/prometheus $pkg_prefix/prometheus 
+  cp ${HAB_CACHE_SRC_PATH}/${pkg_dirname}.linux-amd64/prometheus.yml $pkg_prefix/prometheus
+}
