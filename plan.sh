b2b3e188f8cfd5dbb1f80a173bdafaf6f10d072b
Lamont Lucas <lamont@fastrobot.com>
2017-03-23 02:25:31 -0700

    remove breakpoint

--- plan.sh
+++ plan.sh
@@ -32,5 +32,4 @@ do_install() {
   mkdir ${pkg_prefix}/prom || true
   cp ${HAB_CACHE_SRC_PATH}/${pkg_dirname}.linux-amd64/prometheus $pkg_prefix/prom/prometheus 
   cp ${HAB_CACHE_SRC_PATH}/${pkg_dirname}.linux-amd64/prometheus.yml $pkg_svc_config_path/prometheus.yml
-  attach
 }
