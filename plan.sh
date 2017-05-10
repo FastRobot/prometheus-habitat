3f926055164c9cdd96ea88f98b20ad658572ff91
Lamont Lucas <lamont@fastrobot.com>
2017-05-10 05:37:36 -0700

    got rid of all scaffolding_go and can now build from source with version string in 2m26s

--- plan.sh
+++ plan.sh
@@ -6,28 +6,42 @@ pkg_version=1.6.1
 pkg_maintainer="Lamont Lucas <lamont@fastrobot.com>"
 pkg_license=('Apache-2.0')
 pkg_bin_dirs=(bin)
-pkg_source=github.com/prometheus/prometheus/cmd/prometheus
-pkg_scaffolding=core/scaffolding-go
+pkg_source="https://github.com/prometheus/prometheus/archive/v${pkg_version}.tar.gz"
+pkg_shasum=ecc9ce94fce45994c23b76eb0c5acbb1b942513be601872c8cd74d0821450c5e
+prom_pkg_dir="$HAB_CACHE_SRC_PATH/${pkg_name}-${pkg_version}"
+prom_build_dir="${prom_pkg_dir}/src/${pkg_source}"
+pkg_deps=(
+  core/go
+)
+pkg_build_deps=(
+  core/git
+  core/gcc
+  core/make
+)
 
 pkg_exports=(
   [port-http]=listening_port
 )
 pkg_exposes=(port-http)
-#pkg_svc_run="prometheus -config.file=${pkg_svc_config_path}/prometheus.yml \
-#-web.listen-address :${cfg.ports.http} "
 
-#do_before() {
-#  mkdir -p ${HAB_CACHE_SRC_PATH}/${pkg_dirname}/src
-#}
 
-#do_build() {
-#  make
-#  # return 0
-#}
+do_prepare() {
+  export GOPATH=$HAB_CACHE_SRC_PATH/$pkg_dirname
+}
 
-#do_install() {
-#  attach
-#  mkdir -p ${pkg_prefix}/prom
-#  cp ${HAB_CACHE_SRC_PATH}/${pkg_dirname}.linux-amd64/prometheus $pkg_prefix/prom/prometheus
-#  #cp ${HAB_CACHE_SRC_PATH}/${pkg_dirname}.linux-amd64/prometheus.yml $pkg_svc_config_path/prometheus.yml
-#}
+do_unpack() {
+  mkdir -p $prom_pkg_dir/src/github.com/prometheus/prometheus
+  tar zxf "v${pkg_version}.tar.gz" --strip-components=1 -C ${prom_pkg_dir}/src/github.com/prometheus/prometheus
+}
+
+do_build() {
+  pushd ${prom_pkg_dir}/src/github.com/prometheus/prometheus
+  make build
+  $GOPATH/bin/promu build --prefix $pkg_prefix/bin
+  popd
+  return 0
+}
+
+do_install() {
+  return 0
+}
