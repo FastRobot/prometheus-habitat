9c8c04a1b23cd18f38a199264e5bf0c13887af35
Lamont Lucas <lamont@fastrobot.com>
2017-05-10 02:37:00 -0700

    builds head, albeit stupidly. uses scaffolding_go, which is nice. Version is wrong in the binary, need to update to pick a specific release source ball rather than use go gets from the scaffolding

--- plan.sh
+++ plan.sh
@@ -2,34 +2,32 @@ pkg_name=prometheus
 pkg_description="Prometheus monitoring"
 pkg_upstream_url=http://prometheus.io
 pkg_origin=fastrobot
-pkg_version=1.5.2
+pkg_version=1.6.1
 pkg_maintainer="Lamont Lucas <lamont@fastrobot.com>"
 pkg_license=('Apache-2.0')
-pkg_source=https://github.com/prometheus/prometheus/releases/download/v${pkg_version}/prometheus-${pkg_version}.linux-amd64.tar.gz
-pkg_shasum=971c5f365c3f53f52d05729acf43962905832b33d740798094850a25645de5ae
-pkg_dirname=${pkg_name}-${pkg_version}
-pkg_deps=(core/go)
-pkg_build_deps=(core/tar)
-#pkg_lib_dirs=(lib)
-#pkg_include_dirs=(include)
+pkg_bin_dirs=(bin)
+pkg_source=github.com/prometheus/prometheus/cmd/prometheus
+pkg_scaffolding=core/scaffolding-go
+
 pkg_exports=(
-  [port-http]=ports.http
+  [port-http]=listening_port
 )
 pkg_exposes=(port-http)
-pkg_bin_dirs=(prom)
-pkg_svc_run="prometheus -config.file=$pkg_svc_config_path/prometheus.yml"
+#pkg_svc_run="prometheus -config.file=${pkg_svc_config_path}/prometheus.yml \
+#-web.listen-address :${cfg.ports.http} "
 
-do_unpack() {
-  cd "${HAB_CACHE_SRC_PATH}" || exit
-  tar zxf ${pkg_filename}
-}
+#do_before() {
+#  mkdir -p ${HAB_CACHE_SRC_PATH}/${pkg_dirname}/src
+#}
 
-do_build() {
-  return 0
-}
+#do_build() {
+#  make
+#  # return 0
+#}
 
-do_install() {
-  mkdir -p ${pkg_prefix}/prom 
-  cp ${HAB_CACHE_SRC_PATH}/${pkg_dirname}.linux-amd64/prometheus $pkg_prefix/prom/prometheus 
-  #cp ${HAB_CACHE_SRC_PATH}/${pkg_dirname}.linux-amd64/prometheus.yml $pkg_svc_config_path/prometheus.yml
-}
+#do_install() {
+#  attach
+#  mkdir -p ${pkg_prefix}/prom
+#  cp ${HAB_CACHE_SRC_PATH}/${pkg_dirname}.linux-amd64/prometheus $pkg_prefix/prom/prometheus
+#  #cp ${HAB_CACHE_SRC_PATH}/${pkg_dirname}.linux-amd64/prometheus.yml $pkg_svc_config_path/prometheus.yml
+#}
